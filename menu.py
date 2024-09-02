d86b21a9f5c378464964c623f6e66ab7e395eef8
Peter Jansen <petesterama@gmail.com>
2024-09-02 14:57:03 -0700

    update menu.py

--- menu.py
+++ menu.py
@@ -1,15 +1,15 @@
 import nuke
 import os
 
-# P_FolderPath = os.path.dirname(__file__)
-
-
-nuke.pluginAddPath('./gizmos')
+nuke.pluginAddPath('./tools')
 nuke.pluginAddPath('./icons')
 
-toolbar = nuke.toolbar('Nodes') # The default nuke nodes toolbaron the left hand side of screen
+##########################################################################################################################################
 
+toolbar = nuke.toolbar('Nodes') # The default nuke nodes toolbaron the left hand side of screen
 p_tools = toolbar.addMenu('Peter Tools', icon = 'p.png') # add menu for Peter Tools
+
+##########################################################################################################################################
 
 # Draw
 
@@ -18,7 +18,7 @@ p_tools = toolbar.addMenu('Peter Tools', icon = 'p.png') # add menu for Peter To
 # Filter
 filter = p_tools.addMenu('Filter', icon = 'p.png')
 
-filter.addCommand('Fractal Blur', 'nuke.createNode(\'fractalBlur2.nk\')', icon = 'fractalBlur.png')
+filter.addCommand('Bump Displace', 'nuke.createNode(\'BumpDisplace.nk\')', icon = 'bumpDisplacepng')
 
 # Keying
 
