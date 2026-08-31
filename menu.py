50fe75728b9711fb85af67b39ee5ac28512d2ade
render <render@distilleryvfx.com>
2026-08-31 16:30:53 -0700

    1.3 - preblur highlights and fixed knob names

--- menu.py
+++ menu.py
@@ -53,6 +53,7 @@ filter.addCommand('DiffusionRays', 'nuke.createNode(\'DiffusionRays.nk\')', icon
 filter.addCommand('Confocus', 'nuke.createNode(\'Confocus.nk\')', icon = 'p.png')
 filter.addCommand('InpaintFaster', 'nuke.createNode(\'InpaintFaster.nk\')', icon = 'p.png')
 filter.addCommand('Astigmatism', 'nuke.createNode(\'Astigmatism.nk\')', icon = 'p.png')
+filter.addCommand('ArtLens', 'nuke.createNode(\'ArtLens.nk\')', icon = 'p.png')
 
 p_tools.addSeparator()
 
